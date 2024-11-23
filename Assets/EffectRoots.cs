086ae8e8465dc1e4dfe5740a4f10210a2eefbdc8
Ben Shapiro <bshap93@gmail.com>
2024-11-23 16:12:29 -0700

    Addon to Melee weapon

---
+++ Assets/EffectRoots.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectRoots : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Transform> SwordEffectRootsList;
+    public List<Transform> AxeEffectRootsList;
+}
---
+++ Assets/Project/Gameplay/Combat/Weapons/StrategicMeleeWeapon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using MoreMountains.Tools;
+using UnityEngine;
+
+namespace Project.Gameplay.Combat.Weapons
+{
+    public class MeleeWeaponExtension : MonoBehaviour
+    {
+        // Enum for MeleeWeapon types
+        public enum MeleeWeaponType
+        {
+            Sword,
+            Axe
+        }
+
+        [MMInspectorGroup("Particle Effects", true)]
+        public List<ParticleSystem> AttackParticles; // List of attack particles 
+        public List<ParticleSystem> HitParticles; // List of hit particles
+        [SerializeField] EffectRoots _playerEffectsRoot; // Cache for player's effects root
+
+        [MMInspectorGroup("Attack Variations", true)]
+        public List<string> AttackAnimations; // Animation triggers in the Animator
+        public bool UseRandomAnimation = true; // Toggle for random animations
+        int _currentAnimationIndex = 0;
+
+        protected void Start()
+        {
+            _playerEffectsRoot = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<EffectRoots>();
+        }
+    }
+}
