a6ed38ae031c07bf9a9689654463485ae10c6c90
Ben Shapiro <bshap93@gmail.com>
2024-11-23 17:23:27 -0700

    Sword hits obstacle

--- Assets/Project/Gameplay/Combat/Weapons/StrategicMeleeWeapon.cs
+++ Assets/Project/Gameplay/Combat/Weapons/StrategicMeleeWeapon.cs
@@ -6,17 +6,11 @@ namespace Project.Gameplay.Combat.Weapons
 {
     public class MeleeWeaponExtension : MonoBehaviour
     {
-        // Enum for MeleeWeapon types
-        public enum MeleeWeaponType
-        {
-            Sword,
-            Axe
-        }
-
         [MMInspectorGroup("Particle Effects", true)]
-        public List<ParticleSystem> AttackParticles; // List of attack particles 
-        public List<ParticleSystem> HitParticles; // List of hit particles
+        public List<GameObject> AttackParticlesPrefabs; // List of attack particles 
+        public List<GameObject> HitParticlesPrefabs; // List of hit particles
         [SerializeField] EffectRoots _playerEffectsRoot; // Cache for player's effects root
+        public MeleeWeaponType MWWeaponType; // Weapon type
 
         [MMInspectorGroup("Attack Variations", true)]
         public List<string> AttackAnimations; // Animation triggers in the Animator
@@ -26,6 +20,20 @@ namespace Project.Gameplay.Combat.Weapons
         protected void Start()
         {
             _playerEffectsRoot = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<EffectRoots>();
+        }
+
+        public void PlayBasicAttackParticleEffect()
+        {
+            // Get the current effect root
+            var effectRoot = MWWeaponType == MeleeWeaponType.Sword
+                ? _playerEffectsRoot.SwordEffectRootsList[0]
+                : _playerEffectsRoot.AxeEffectRootsList[0];
+
+            // Instantiate the attack particle effect
+            var attackParticle = Instantiate(AttackParticlesPrefabs[0], effectRoot.position, effectRoot.rotation);
+            attackParticle.transform.SetParent(effectRoot);
+
+            attackParticle.GetComponent<ParticleSystem>().Play();
         }
     }
 }
