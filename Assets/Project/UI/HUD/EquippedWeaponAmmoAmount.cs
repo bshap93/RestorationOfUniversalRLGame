1a60e14e77c436ee3ee36ad03e4f810e25e5ad67
Ben Shapiro <bshap93@gmail.com>
2024-12-10 14:16:56 -0700

    Adjusting heirarchy

---
+++ Assets/Project/UI/HUD/EquippedWeaponAmmoAmount.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Project.UI.HUD
+{
+    public class EquippedWeaponAmmoAmount : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            gameObject.SetActive(false);
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
