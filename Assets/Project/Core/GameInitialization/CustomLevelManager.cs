ea9571818680129754248c68afb365d1f1376d2f
Ben Shapiro <bshap93@gmail.com>
2024-12-11 17:57:39 -0700

    Dissapearing dungeon?

---
+++ Assets/Project/Core/GameInitialization/CustomLevelManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using DunGen;
+using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+namespace Project.Core.GameInitialization
+{
+    public class CustomLevelManager : LevelManager
+    {
+        [Tooltip("the Room prefav with the checkpoint to use as initial spawn point if no point of entry is specified")]
+
+        RuntimeDungeon runtimeDungeon;
+
+        List<GameObject> EntryCheckpoints = new List<GameObject>();
+
+        protected override void Awake()
+        {
+            runtimeDungeon = GetComponent<RuntimeDungeon>();
+            runtimeDungeon.Generator.OnGenerationStatusChanged += OnDungeonGenerationStatusChanged;
+        }
+
+        protected override void Start()
+        {
+            // Do nothing 
+        }
+
+        void OnDestroy()
+        {
+            runtimeDungeon.Generator.OnGenerationStatusChanged -= OnDungeonGenerationStatusChanged;
+        }
+
+        void OnDungeonGenerationStatusChanged(DungeonGenerator generator, GenerationStatus status)
+        {
+            if (status == GenerationStatus.Complete)
+            {
+                
+                StartCoroutine(InitializationCoroutine());
+            }
+        }
+
+
+        /// <summary>
+        ///     Instantiate playable characters based on the ones specified in the PlayerPrefabs list in the LevelManager's
+        ///     inspector.
+        /// </summary>
+        protected override void InstantiatePlayableCharacters()
+        {
+            _initialSpawnPointPosition =
+                InitialSpawnPoint == null ? Vector3.zero : InitialSpawnPoint.transform.position;
+
+            Players = new List<Character>();
+
+            if (GameManager.Instance.PersistentCharacter != null)
+            {
+                Players.Add(GameManager.Instance.PersistentCharacter);
+                return;
+            }
+
+            // we check if there's a stored character in the game manager we should instantiate
+            if (GameManager.Instance.StoredCharacter != null)
+            {
+                var newPlayer = Instantiate(
+                    GameManager.Instance.StoredCharacter, _initialSpawnPointPosition, Quaternion.identity);
+
+                newPlayer.name = GameManager.Instance.StoredCharacter.name;
+                Players.Add(newPlayer);
+                return;
+            }
+
+            if (SceneCharacters != null && SceneCharacters.Count > 0)
+            {
+                foreach (var character in SceneCharacters) Players.Add(character);
+                return;
+            }
+
+            if (PlayerPrefabs == null) return;
+
+            // player instantiation
+            if (PlayerPrefabs.Length != 0)
+                foreach (var playerPrefab in PlayerPrefabs)
+                {
+                    var newPlayer = Instantiate(playerPrefab, _initialSpawnPointPosition, Quaternion.identity);
+                    newPlayer.name = playerPrefab.name;
+                    Players.Add(newPlayer);
+
+                    if (playerPrefab.CharacterType != Character.CharacterTypes.Player)
+                        Debug.LogWarning(
+                            "LevelManager : The Character you've set in the LevelManager isn't a Player, which means it's probably not going to move. You can change that in the Character component of your prefab.");
+                }
+        }
+    }
+}
