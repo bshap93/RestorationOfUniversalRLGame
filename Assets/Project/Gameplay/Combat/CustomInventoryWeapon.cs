3ed47d5b9473253c120a4748e274e7ff8e04c76d
Ben Shapiro <bshap93@gmail.com>
2024-12-17 13:51:33 -0700

    Variation on the InventoryWeapon

---
+++ Assets/Project/Gameplay/Combat/CustomInventoryWeapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MoreMountains.TopDownEngine;
+using Project.Gameplay.Combat.Weapons;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Project.Gameplay.Combat
+{
+    [CreateAssetMenu(
+        fileName = "CustomInventoryWeapon", menuName = "Roguelike/Weapons/CustomInventoryWeapon", order = 3)]
+    [Serializable]
+    public class CustomInventoryWeapon : InventoryWeapon
+    {
+        [FormerlySerializedAs("WeaponAttachmentType")]
+        public WeaponAttachmentType weaponAttachmentType;
+        public AnimatorOverrideController overrideController;
+    }
+}
