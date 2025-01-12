51096f5a99baac5f07f80a7f36d94d598bb4f517
Ben Shapiro <bshap93@gmail.com>
2025-01-11 17:44:48 -0700

    Making progress on Recipe infos

---
+++ Assets/MaterialEntry.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Project.Gameplay.ItemManagement.InventoryTypes.Materials;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaterialEntry : MonoBehaviour
+{
+    public TMP_Text materialName;
+    public Image materialImage;
+    public TMP_Text materialQuantity;
+    
+    public void SetMaterial(CraftingMaterial material)
+    {
+        materialName.text = material.item.ItemName;
+        materialImage.sprite = material.item.Icon;
+        materialQuantity.text = material.quantity.ToString();
+    }
+
+}
