65bfc83b9f301825f7f68f380782a1a92dbc4587
Ben Shapiro <bshap93@gmail.com>
2025-01-31 11:28:19 -0700

    Cooking not start

--- Assets/Gameplay/ItemManagement/CustomDepositInventoryDisplay.cs
+++ Assets/Gameplay/ItemManagement/CustomDepositInventoryDisplay.cs
@@ -1,18 +1,44 @@
-﻿using MoreMountains.InventoryEngine;
-using Project.UI.Crafting.Cooking;
+﻿using Gameplay.ItemManagement.InventoryTypes;
+using MoreMountains.InventoryEngine;
+using Project.Gameplay.Interactivity.Items;
+using UnityEngine;
 
 namespace Gameplay.ItemManagement
 {
     public class CustomDepositInventoryDisplay : InventoryDisplay
 
     {
-        CookStationPanelInstance _cookStationPanelInstance;
+        public string MainInventoryName = MainInventory.MainInventoryObjectName;
+
+        protected Inventory _mainInventory;
 
         protected override void Awake()
         {
-            _cookStationPanelInstance = GetComponentInParent<CookStationPanelInstance>();
-            TargetInventoryName = _cookStationPanelInstance.cookingStationController.GetDepositInventory().name;
             base.Awake();
+            _mainInventory = FindMainInventory();
+        }
+
+        protected Inventory FindMainInventory()
+        {
+            foreach (var inventory in FindObjectsOfType<Inventory>())
+                if (inventory.name == MainInventoryName && inventory.PlayerID == PlayerID)
+                    return inventory;
+
+            Debug.LogWarning("MainPlayerInventory not found!");
+            return null;
+        }
+
+        public void OnRightClick(InventorySlot slot)
+        {
+            if (_mainInventory == null || slot == null || InventoryItem.IsNull(slot.CurrentItem)) return;
+
+            // Try moving the item back to the main inventory
+            var success = TargetInventory.MoveItemToInventory(slot.Index, _mainInventory);
+
+            if (success)
+                Debug.Log("Item moved back to MainPlayerInventory.");
+            else
+                Debug.Log("Failed to move item back to MainPlayerInventory.");
         }
     }
 }
---
+++ Assets/Gameplay/ItemManagement/UI/RightClickHandler.cs
@@ -0,0 +1,25 @@
+﻿using MoreMountains.InventoryEngine;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Gameplay.ItemManagement.UI
+{
+    public class RightClickHandler : MonoBehaviour, IPointerClickHandler
+    {
+        CustomDepositInventoryDisplay _customDepositInventoryDisplay;
+
+        void Start()
+        {
+            _customDepositInventoryDisplay = gameObject.GetComponentInParent<CustomDepositInventoryDisplay>();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Right && _customDepositInventoryDisplay != null)
+            {
+                var slot = GetComponent<InventorySlot>();
+                _customDepositInventoryDisplay.OnRightClick(slot);
+            }
+        }
+    }
+}
