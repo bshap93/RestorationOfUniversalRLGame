cd672ab390921c8819f627a1b2c70ced88c0fe5a
Ben Shapiro <bshap93@gmail.com>
2025-02-01 12:59:02 -0700

    Static

---
+++ Assets/Project/Editor/Utilities/StaticMeshChecker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Project.Editor.Utilities
+{
+    public class StaticMeshChecker : EditorWindow
+    {
+        void OnGUI()
+        {
+            if (GUILayout.Button("Check Mesh Static Status")) CheckMeshes();
+        }
+        [MenuItem("Debug/Check Static Meshes")]
+        public static void ShowWindow()
+        {
+            GetWindow<StaticMeshChecker>("Static Mesh Checker");
+        }
+
+        static void CheckMeshes()
+        {
+            var allObjects = FindObjectsOfType<GameObject>();
+
+            Debug.Log("=== Static Meshes ===");
+            foreach (var obj in allObjects)
+                if (obj.isStatic && obj.GetComponent<MeshRenderer>())
+                    Debug.Log(obj.name + " is Static", obj);
+
+            Debug.Log("=== Dynamic Meshes ===");
+            foreach (var obj in allObjects)
+                if (!obj.isStatic && obj.GetComponent<MeshRenderer>())
+                    Debug.Log(obj.name + " is Dynamic", obj);
+        }
+    }
+}
