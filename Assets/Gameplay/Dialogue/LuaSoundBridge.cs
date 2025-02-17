7a13b1e91fb4c2b9dbffe514c2f95f4a0c1557e1
Ben Shapiro <bshap93@gmail.com>
2025-02-16 20:43:09 -0700

    Home work

---
+++ Assets/Gameplay/Dialogue/LuaSoundBridge.cs
@@ -0,0 +1,28 @@
+﻿using MoreMountains.Tools;
+using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+namespace Gameplay.Dialogue
+{
+    public class LuaSoundBridge : MonoBehaviour
+    {
+        void Start()
+        {
+            // Register Lua function to play sounds
+            Lua.RegisterFunction("PlayMMSound", this, SymbolExtensions.GetMethodInfo(() => PlayMMSound("")));
+        }
+
+        public static void PlayMMSound(string soundName)
+        {
+            AudioClip clip = Resources.Load<AudioClip>(soundName); // Ensure your sound is in Resources
+            if (clip != null && MMSoundManager.Instance != null)
+            {
+                MMSoundManager.Instance.PlaySound(clip, MMSoundManager.MMSoundManagerTracks.Sfx, Vector3.zero);
+            }
+            else
+            {
+                Debug.LogWarning($"Sound {soundName} not found or MMSoundManager not initialized!");
+            }
+        }
+    }
+}
