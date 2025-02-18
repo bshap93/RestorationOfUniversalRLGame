851593d580693ec37dd62b51c9acc076181abdf1
Ben Shapiro <bshap93@gmail.com>
2025-02-18 14:46:55 -0700

    Trying to get destroyed persistence

---
+++ Assets/Gameplay/Environment/DestructibleHealth.cs
@@ -0,0 +1,42 @@
+﻿using MoreMountains.TopDownEngine;
+using PixelCrushers;
+using UnityEngine;
+
+namespace Gameplay.Environment
+{
+    public class DestructibleHealth : Health
+    {
+        DestructibleSaver _destructibleSaver;
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _destructibleSaver = GetComponent<DestructibleSaver>();
+        }
+
+        protected override void DestroyObject()
+        {
+            // If we have save components, make sure to record destruction before deactivating
+            if (_destructibleSaver != null)
+            {
+                Debug.Log("DestructibleHealth.DestroyObject: Recording destruction");
+                _destructibleSaver.RecordDestruction();
+            }
+
+            if (_autoRespawn == null)
+            {
+                if (DestroyOnDeath)
+                {
+                    if (_character != null)
+                        Destroy(_character.gameObject);
+                    else
+                        Destroy(gameObject);
+                }
+            }
+            else
+            {
+                _autoRespawn.Kill();
+            }
+        }
+    }
+}
